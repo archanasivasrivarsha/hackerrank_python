9bc698a9d5151dd48c2c7285fff17d602dd31278
archanasivasrivarsha <charchana2007@gmail.com>
2026-03-27 09:24:21 +0530

    Add files via upload

---
+++ triangle quest 2.py
@@ -0,0 +1,4 @@
+Python 3.13.12 (tags/v3.13.12:1cbe481, Feb  3 2026, 18:22:25) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> for i in range(1, int(input()) + 1):
+...     print((10**i // 9) ** 2)
